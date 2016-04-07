97ec57d783ff3dc9e78189c27e099c5cd6086ed2
Bartosz <bartekhamielec@gmail.com>
2016-04-07 22:58:44 +0200

    Hope this is what i've made is working Forward checking

--- CSPRun.py
+++ CSPRun.py
@@ -5,6 +5,7 @@ import time
 
 sudoku9x9 = DataProvider.generateSudoku9x9(6)
 start = time.time()
-solved = SudokuSolver.solve9x9(sudoku9x9)
+#solved = SudokuSolver.solve9x9Backtracking(sudoku9x9)
+solved = SudokuSolver.solve9x9ForwardCheckingNew(DataProvider.sudoku9x9Normal)
 end = time.time()
 print "Time = ", end-start
--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,10 +1,13 @@
 import numpy
+import numpy as np
+from operator import or_
+
 sudokuSize = 9
 
 
 
 
-def solve9x9(grid):
+def solve9x9Backtracking(grid):
     row, column = findUnassignedPlaces(grid)
     if row == -2 and column == -2:
         showSolvedGrid(grid)
@@ -14,10 +17,136 @@ def solve9x9(grid):
         if isCorrect(grid, row, column, proposedNumber):
             grid[row,column] = proposedNumber
 
-            if solve9x9(grid):
+            if solve9x9Backtracking(grid):
                 return True
             grid[row,column] = 0
     return False
+
+
+def forwardCheck(grid, row, column): # return False if does not have a conflict
+    loopIter = True
+    tempGrid = grid.copy()
+    while loopIter:
+        row, column = findUnassignedPlaces(tempGrid)
+        if row == -2 and column == -2:
+            return False
+        actualDomain = getActualDomain(tempGrid, row,column)
+        if actualDomain == []:
+            return True
+        else:
+            tempGrid[row,column] = actualDomain[0]
+
+
+
+
+def solve9x9ForwardChecking(grid):
+    row, column = findUnassignedPlaces(grid)
+    if row == -2 and column == -2:
+        showSolvedGrid(grid)
+        return True  # solution found!
+    actualDomain = getActualDomain(grid, row, column)
+    for proposedNumber in actualDomain:
+        if isCorrect(grid, row, column, proposedNumber):
+            grid[row,column] = proposedNumber
+            if forwardCheck(grid, row, column):
+                grid[row, column] = 0
+            if solve9x9ForwardChecking(grid):
+                return True
+            grid[row,column] = 0
+    return False
+
+
+
+def solve9x9ForwardCheckingNew(grid):
+    row, column = findUnassignedPlaces(grid)
+    if row == -2 and column == -2 :
+        showSolvedGrid(grid)
+        return True  # solution found!
+    actualDomain = getActualDomain(grid, row, column)
+    for proposedNumber in actualDomain:
+        grid[row,column] = proposedNumber
+        domainWipeOut = False
+        for variable in getUnassignedFromConstraints(grid.copy(), row, column):
+            if fc(variable.grid, variable.row, variable.column):
+                domainWipeOut = True
+                break
+        if not domainWipeOut:
+            solve9x9ForwardCheckingNew(grid)
+        grid[row,column] = 0
+
+
+def getUnassignedInRow(grid, row):
+    result = []
+    columnCounter = -1
+    for variable in grid[row]:
+        columnCounter += 1
+        if variable == 0:
+            result.append(UnassignedVariableFromConstrain(row, columnCounter, grid.copy()))
+    return set(result)
+
+
+def getUnassignedInColumn(grid, column):
+    result = []
+    rowCounter = -1
+    for variable in grid[:,column]:
+        rowCounter += 1
+        if variable == 0:
+            result.append(UnassignedVariableFromConstrain(rowCounter, column, grid.copy()))
+    return set(result)
+
+
+def getUnassignedInSquare(grid, rowStart, columnStart):
+    result = []
+    if rowStart == 0:
+        rowStart = 1
+    if columnStart == 0:
+        columnStart = 1
+    for row in range(0,3):
+        for col in range(0,3):
+            if grid[rowStart+row,columnStart+col] == 0:
+                result.append(UnassignedVariableFromConstrain(row+rowStart, col+columnStart, grid.copy()))
+    return set(result)
+
+
+def getUnassignedFromConstraints(grid, row, column):
+    unassignedInRow = getUnassignedInRow(grid,row)
+    unassignedInColumn = getUnassignedInColumn(grid, column)
+    unassignedInSquare = getUnassignedInSquare(grid, row - row%3, column - column%3)
+    unassigneds = unassignedInSquare.symmetric_difference(unassignedInColumn)
+    unassigneds = unassigneds.symmetric_difference(unassignedInRow)
+    return unassigneds
+
+
+def fc(grid, row, column):
+    actualDomain = getActualDomain(grid, row, column)
+    domainCopy = list(actualDomain)
+    for domainProposition in actualDomain:
+        if not isCorrect(grid, row, column, domainProposition):
+            domainCopy.remove(domainProposition)
+    return len(domainCopy) == 0
+
+
+
+
+
+def getActualDomain(grid, row, column):
+    resultList = set(range(1,10))
+    wholeRow = set(grid[row,:])- set([0])
+    wholeColumn = set(grid[:,column])- set([0])
+    wholeSquare = getSquare(grid, row - row%3, column - column%3) - set([0])
+    reducedEliminatedSet = reduce(or_, [wholeRow, wholeColumn, wholeSquare])
+    resultList = resultList.symmetric_difference(reducedEliminatedSet)
+    return list(resultList)
+
+def getSquare(grid, row, column):
+    squareDomain = []
+    tempIter = 1
+    for tempRow in range(row, row+3):
+        for tempCol in range(column, column+3):
+            squareDomain.append(grid[tempRow, tempCol])
+    return set(squareDomain)
+
+
 
 
 def findUnassignedPlaces(grid):
@@ -58,3 +187,17 @@ def isCorrectInBox(grid, rowStart, columnStart, proposedNumber):
 
 def showSolvedGrid(grid):
     print grid
+
+
+class UnassignedVariableFromConstrain:
+
+    def __init__(self, row, column, grid):
+        self.row = row
+        self.column = column
+        self.grid = grid
+
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+
+    def __hash__(self):
+        return hash(self.row) ^ hash(self.column)
